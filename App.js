422bc45af35cb9b2c2c20c08e3ced611735e2a3e
Logan Banning <loganboganb@gmail.com>
2022-08-18 19:46:08 -0600

    finished

--- App.js
+++ App.js
@@ -1,90 +1,92 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import './App.css';
-
-const books = {
-  title: 'The Lord Of The Flies',
-  id: 4345,
-  description: "William Golding's 1954 novel Lord of the Flies tells the story of a group of young boys who find themselves alone on a deserted island",
-  authors: [
-    123,
-    456
-  ]
- }
-// {
-//   title: 'Harry Potter',
-//   id: 4346,
-//   description: "Magical kid",
-//   authors: [
-//     234,
-//     567
-//   ]
-// },
-// {
-//   title: 'Money Moves',
-//   id: 4347,
-//   description: "Making money moves",
-//   authors: [
-//     879,
-//     637
-//   ]
-// }]
-
-const authorInfo = {
-  id: 456,
-  firstName: 'Joshua',
-  lastName: 'Beatty'
-}
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 const FetchData = () => {
-  const [search, setSearch] = useState('');
-  const [result, setResult] = useState([]);
-  const [author, setAuthor] = useState([]);
+  const [search, setSearch] = useState("");
+  const [book, setBook] = useState({});
+  const [authors, setAuthors] = useState([]);
+  const [error, setError] = useState("");
 
   const handleSearch = () => {
+    setBook({});
+    setAuthors([]);
+    bookSearch();
+    authorSearch();
+    setSearch("");
+  };
+
+  const bookSearch = () => {
     try {
-      axios.post('https://ejditq67mwuzeuwrlp5fs3egwu0yhkjz.lambda-url.us-east-2.on.aws/api/books/search', {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        data: {
-          "title": `${search}`
-        }
-      }).then((res) => {
-        setResult(res.data);
-      })
-      axios.get(`https://ejditq67mwuzeuwrlp5fs3egwu0yhkjz.lambda-url.us-east-2.on.aws/api/authors/:${result.authors[0]}`, {
-        headers: {
-          "Content-Type": 'application/json'
-        }
-      }).then((res) => {
-        setAuthor(res.data);
-      })
+      axios
+        .post(
+          "https://ejditq67mwuzeuwrlp5fs3egwu0yhkjz.lambda-url.us-east-2.on.aws/api/books/search",
+          {
+            headers: {
+              "Access-Control-Allow-Headers": "true",
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Credentials": "true",
+            },
+            data: {
+              title: `${search}`,
+            },
+          }
+        )
+        .then((res) => {
+          setBook(res.data);
+        });
     } catch (err) {
-      setResult(books);
-      setAuthor(authorInfo);
-      setSearch('');
+      setError("Book Not Found");
     }
-  }
-  console.log(result);
-  console.log(author);
+  };
+
+  const authorSearch = () => {
+    if(book.authors && book.authors.length > 0){
+      book.authors.forEach((id) => {
+        try {
+          axios
+            .get(
+              `https://ejditq67mwuzeuwrlp5fs3egwu0yhkjz.lambda-url.us-east-2.on.aws/api/authors/${id}`,
+              {
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            )
+            .then((res) => {
+              setAuthors([...authors, res.data]);
+            });
+        } catch (err) {
+          setError("Author Not Found");
+        }
+      });
+    };
+    }; 
 
   return (
-    <div className='main-page'>
-      <div className='search-container'>
+    <div className="main-page">
+      <div className="search-container">
         <h1>Books and Authors</h1>
-        <input type='text' 
-        placeholder='Search' 
-        onChange={(e) => setSearch(e.target.value)}></input>
+        <input
+          type="text"
+          placeholder="Search Books"
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
         <button onClick={handleSearch}>Search</button>
       </div>
-      <div className='author-info'>
-        <h2>{result.title}</h2>
-        <div>{result.description}</div>
-        <h3>{author.firstName} {author.lastName}</h3>
+      <div className="author-info">
+        <h2>{book.title}</h2>
+        <div>{book.description}</div>
+          {authors.map(({firstName, middleInitial, lastName}) => {
+            return (
+              <h3>{firstName} {middleInitial ?? ''} {lastName}</h3>
+            )
+          })}
+        {error && <div>{error}</div>}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default FetchData;
